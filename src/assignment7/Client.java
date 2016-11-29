51799ffe17b5aa10f45b69c428ca4b6ecaa92b61
jdbravol <jdbravol@utexas.edu>
2016-11-29 03:02:11 -0600

    server change

--- src/assignment7/Client.java
+++ src/assignment7/Client.java
@@ -7,8 +7,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -32,70 +34,62 @@ public class Client extends Application{
     private BufferedReader reader;
     private PrintWriter writer;
 
-    private void initView(){
+    private void initView(Stage primaryStage){
         //Initial Login page
+        BorderPane paneForUser = new BorderPane();
+        paneForUser.setPadding(new Insets(5, 5, 5, 5));
+        paneForUser.setLeft(new Label("User Name: "));
+
+        TextField username = new TextField();
+        username.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForUser.setCenter(username);
+
+        BorderPane paneForPass = new BorderPane();
+        paneForPass.setPadding(new Insets(5, 5, 5, 5));
+        paneForPass.setLeft(new Label("Password: "));
+
+        TextField password = new TextField();
+        password.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForUser.setCenter(password);
+
+        BorderPane mainPane = new BorderPane();
+        // Text area to display contents
+
+        mainPane.setTop(paneForUser);
+        mainPane.setCenter(paneForPass);
 
 
-        Stage loginStage = new Stage();
-        GridPane mainPanel = new GridPane();
-        Scene loginScene;
-        //Username & Password input
-        Label userName = new Label("User Name: ");
-        TextField user = new TextField();
-        Label pass = new Label("Password: ");
-        PasswordField password = new PasswordField();
-        mainPanel.add(userName, 0, 0);
-        mainPanel.add(user, 0, 1);
-        mainPanel.add(pass, 1, 0);
-        mainPanel.add(password, 1, 1);
-
-
-        //buttons
-        Button newUser = new Button("Create User");
-        Button forgotPass = new Button("forgot Password");
-        Button login = new Button("Login");
-        mainPanel.add(newUser, 2, 0);
-        mainPanel.add(login, 2, 1);
-        mainPanel.add(forgotPass, 3, 0);
-
-        //set Handlers
-        login.setOnAction(e ->{
-            //TODO
-            try {
-
-            }
-            catch (Exception ex){
-
-            }
-        });
-
-        newUser.setOnAction(e ->{
-            //TODO
-            try {
-
-            }
-            catch (Exception ex){
-
-            }
-        });
-
-        forgotPass.setOnAction(e ->{
-            //TODO
-            try {
-
-            }
-            catch (Exception ex){
-
-            }
-        });
-
-
-        // Putting everything together
-        loginStage.setTitle("Welcome to Messenger");
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(mainPane, 450, 200);
+        primaryStage.setTitle("Client"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
 
     }
 
-    private void MessengerView() {
+    private void MessengerView(Stage primaryStage) {
+        BorderPane paneForTextField = new BorderPane();
+        paneForTextField.setPadding(new Insets(5, 5, 5, 5));
+        paneForTextField.setStyle("-fx-border-color: green");
+        paneForTextField.setLeft(new Label("Enter a radius: "));
+
+        TextField tf = new TextField();
+        tf.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForTextField.setCenter(tf);
+
+        BorderPane mainPane = new BorderPane();
+        // Text area to display contents
+
+        TextArea ta = new TextArea();
+        mainPane.setCenter(new ScrollPane(ta));
+        mainPane.setTop(paneForTextField);
+
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(mainPane, 450, 200);
+        primaryStage.setTitle("Client"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
 
 
     }
@@ -116,9 +110,9 @@ public class Client extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        initView();
+        initView(primaryStage);
         setUpNetworking();
-        MessengerView();
+        MessengerView(primaryStage);
     }
 
     class SendButtonListener implements ActionListener {
